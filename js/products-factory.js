fd4d85d73aad438f905c7964fbd4760abaffa8b2
krysmathis <krysmathis@gmail.com>
2017-10-12 13:41:13 -0500

    finished images and added float styling

--- js/products-factory.js
+++ js/products-factory.js
@@ -10,25 +10,25 @@ const product = {
 } 
 */
 
-const leash = {
-    "name": "Adorable, Adjustable Ferret Leash",
-    "type": "training",
-    "description": "Keep your Mustela putorius furo as near or far as you'd like",
-    "img": "http://m.rgbimg.com/cache1odixs/users/j/ju/just4you/600/mqyv1jg.jpg",
-    "price": "$25.00",
-    "userStory": "My dear Evaline loves to scamper about. With this little gem I can quickly and easily return her to my arms.",
-    "author": "author1",
+const scarf = {
+    "name": "Adorable, Hand-Knitted Icelandic Scarf",
+    "type": "clothing",
+    "description": "Keep your Mustela putorius furo warm as she makes your heart",
+    "img": "./images/km-ferret-scarf.jpg",
+    "price": "$225.00",
+    "userStory": "My dear Evaline loves to scamper about in the snow. With this beautiful scarf she stays as warm as when she's in my arms.",
+    "author": "Raj Erle",
     "sku": "0001"
 }
 
-const bowlerHat = {
+const topHat = {
     "name": "Symphony Style Top Hat",
-    "type": "comfort",    
+    "type": "clothing",    
     "description": "Based on Edward Coke's, younger brother of the 2nd Earl of Leicester, original design, this clever cranial accoutremont is the definition of the classically dressed ferret gentleman",
-    "img": "https://s7d5.scene7.com/is/image/PetsUnited/T650691?wid=250&hei=282",
+    "img": "./images/km-ferret-top-hat.gif",
     "price": "$125.00",
     "userStory": "When my Charlie saw this online he wouldn't stop biting my feet until he had it. There were several days of pain involved, but totally worth it.",
-    "author": "author2",
+    "author": "Erastos Anastastius",
     "sku": "0002" 
 }
 
@@ -36,10 +36,10 @@ const book = {
     "name": "From Ferret Wild to Ferret Child",
     "type": "training",
     "description": "From acclaimed ferret wrangler, Christine D'Chentour.  This book will help you rediscover your own childlike nature.  Become the catalyst that elevates your companion's emotional well-being and intellectual reasoning to new heights.",
-    "img": "https://s7d5.scene7.com/is/image/PetsUnited/T650691?wid=250&hei=282",
+    "img": "./images/km-book-ferret.jpg",
     "price": "$32.00",
     "userStory": "Life Changing",
-    "author": "author1",
+    "author": "Felicitas Loris",
     "sku": "0003"
 }
 
@@ -47,10 +47,10 @@ const bed  = {
     "name": "Comfortable, stylish day-bed",
     "type": "comfort",
     "description": "Nap with an art-deco touch. Your burrowing companion will find a lot to love within this Frank Lloyd Write inspired ferret day bed",
-    "img": "https://s7d5.scene7.com/is/image/PetsUnited/T650691?wid=250&hei=282",
-    "price": "$1,200.00",
+    "img": "./images/km-ferret-beds-hand-carved.jpg",
+    "price": "$12,000.00",
     "userStory": "I wish I was a ferret",
-    "author": "author1",
+    "author": "Gillean Rinaldo",
     "sku": "0004"
 }
 
@@ -58,10 +58,10 @@ const tubes = {
     "name": "Exotic Excursion Tunnels",
     "type": "exercise",
     "description": "Based on Bay Area artist Tom Franco's recent exhibition. These tubes will challenge your ferret's reflexes and artistic sensibilities",
-    "img": "https://s7d5.scene7.com/is/image/PetsUnited/T650691?wid=250&hei=282",
+    "img": "./images/km-products-pipes.jpg",
     "price": "$3,600.00",
-    "userStory": "I wish I was a ferret",
-    "author": "author1",
+    "userStory": "My ferret lost his mind and his pre-concieved notions of what life is and how to live it.",
+    "author": "Noemia Vlahovic",
     "sku": "0005"
 }
 
@@ -72,12 +72,14 @@ const ShoppingCart = {
     "items": []
 }
 
-const training = [leash, book];
-const comfort = [bowlerHat, bed];
+const training = [book];
+const clothing = [scarf, topHat]
+const comfort = [bed];
 const exercise = [tubes];
 
 const Products = {
     "training": training,
+    "clothing": clothing,
     "comfort": comfort,
     "exercise": exercise
 }
