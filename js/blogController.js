0f16f4b883f727781129ef71fd754810c028a47f
Christopher Miller <camiller.yr@gmail.com>
2017-10-12 13:23:15 -0500

    Mid point commit?

---
+++ js/blogController.js
@@ -0,0 +1,25 @@
+let blogDatabaseController = JSON.parse(blogDatabaseString)
+
+console.log(blogDatabaseController)
+
+let blogPreviewPopulated = document.getElementById("blogPreviews")
+
+for (let index = 0; index < blogDatabaseController.length; index++) {
+    
+    let object = blogDatabaseController[index]
+
+    let imgPosition = "right"
+
+    if (index % 2 === 0) {imgPosition = "left"}
+    
+    blogPreviewPopulated.innerHTML += `
+    <div class="card">
+        <img class="${imgPosition}" src="${object.image}">
+        <div class="blog_title">
+            <p>${object.title}</p>
+        </div>
+    </div>
+    `
+        
+    }
+    
